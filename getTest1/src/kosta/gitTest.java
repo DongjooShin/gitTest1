a808d9327ca0e8e80a5c74497f4a9d34856689a4
Dongjoo Shin <Dongjoo Shin@DongjooShin-PC>
2016-04-19 23:53:03 +0900

    added a second test

--- getTest1/src/kosta/gitTest.java
+++ getTest1/src/kosta/gitTest.java
@@ -6,6 +6,9 @@ public class gitTest {
 		System.out.println("HELLO");
 		System.out.println("First Hello");
 		System.out.println("Second Hello");
+		
+		System.out.println("test1");
+		System.out.println("test2");
 
 	}
 
